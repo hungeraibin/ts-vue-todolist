d6f9e1076374b0de96e6d8674db509374893115e
王爱彬 <hungeraibin@126.com>
2019-04-11 17:09:34 +0800

    完成TodoList

---
+++ src/components/TodoList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(todoItem, index) in list" :key="index">
+        <input type="checkbox" :checked="todoItem.status === 'done'" @change="changeStatus(todoItem, $event)">
+        {{ todoItem.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Todo from '../models/Todo'
+
+@Component({
+  props: {
+    list: Array
+  }
+})
+
+export default class TodoList extends Vue {
+  changeStatus(todoItem: Todo, e: Event) {
+    let checked = (<HTMLInputElement>e.target).checked
+    this.$emit('updateTodo', todoItem, { status: checked ? 'done' : 'todo' })
+  }
+}
+</script>
+
